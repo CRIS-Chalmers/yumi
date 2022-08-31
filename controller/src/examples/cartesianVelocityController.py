56087379d41355fad47ee15a8de4d4cbac6f22a2
Gabriel <gabwal@chalmers.se>
2022-08-31 13:30:00 +0200

    Changed msg type for cartesian control

--- controller/src/examples/cartesianVelocityController.py
+++ controller/src/examples/cartesianVelocityController.py
@@ -6,8 +6,10 @@ import os
 import sys
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 from Controller.controller import YumiController
-from controller.msg import CartesianVelocityControl_msg
+#from controller.msg import CartesianVelocityControl_msg
 from controller.msg import PositionAndVelocity_msg
+from std_msgs.msg import Float32MultiArray
+
 import numpy as np
 
 
@@ -20,8 +22,8 @@ class Controller(YumiController):
         # [rightEndTranslation [m/s], rightEndRotation[rad/s], leftEndTranslation, leftEndRotation]
         self.velocity = np.zeros(12)
 
-        self.rightGripper = 0
-        self.leftGripper = 0
+        self.rightGripper = -1
+        self.leftGripper = -1
         self.newGripperLeft = False
         self.newGripperRight = False
         
@@ -29,7 +31,10 @@ class Controller(YumiController):
         self.velocity_pub = rospy.Publisher('/yumi/egm/positionAndVelocity', PositionAndVelocity_msg, 
                                             queue_size=1, tcp_nodelay=True)
 
-        rospy.Subscriber("/yumi/egm/cartesianVelocityCommand", CartesianVelocityControl_msg, self.cartesianVelCallback, 
+        #rospy.Subscriber("/yumi/egm/cartesianVelocityCommand", CartesianVelocityControl_msg, self.cartesianVelCallback, 
+        #                 queue_size=3, tcp_nodelay=True)
+
+        rospy.Subscriber("/yumi/egm/cartesianVelocityCommand", Float32MultiArray, self.cartesianVelCallback, 
                          queue_size=3, tcp_nodelay=True)
 
     def policy(self):
@@ -80,16 +85,22 @@ class Controller(YumiController):
         self.velocity_pub.publish(msg)
 
     def cartesianVelCallback(self, data):
-        linRight = data.linearVelocityRight
-        angRight = data.angularVelocityRight
-
-        linLeft = data.linearVelocityLeft
-        angLeft = data.angularVelocityLeft
+        command_list = data.data
+        #linLeft = data.linearVelocityLeft
+        #linRight = data.linearVelocityRight
+        #angLeft = data.angularVelocityLeft
+        #angRight = data.angularVelocityRight
+        linLeft = command_list[0:3]
+        linRight = command_list[3:6] 
+        angLeft = command_list[6:9]
+        angRight = command_list[9:12]
+        gripperLeft =command_list[12]
+        gripperRight =command_list[13]
 
         self.velocity = np.hstack([linRight, angRight, linLeft, angLeft])
 
         self.lastTime = rospy.Time.now().to_sec()
-
+        '''
         if data.gripperLeft != self.leftGripper:
             self.leftGripper = data.gripperLeft
             self.newGripperLeft = True
@@ -97,7 +108,15 @@ class Controller(YumiController):
         if data.gripperRight != self.rightGripper:
             self.rightGripper = data.gripperRight
             self.newGripperRight = True
+        '''
+        if gripperLeft != self.leftGripper:
+            self.leftGripper = gripperLeft
+            self.newGripperLeft = True
 
+        if gripperRight != self.rightGripper:
+            self.rightGripper = gripperRight
+            self.newGripperRight = True
+        
 
 def main():
     # starting ROS node and subscribers
